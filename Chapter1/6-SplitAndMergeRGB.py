5769653db1b7fefbbec5eca33d5aa19a7df27213
kartik khandelwal <khandelwal.kartik4@gmail.com>
2018-01-18 22:35:38 +0530

    R G B individual images

--- Chapter1/6-SplitAndMergeRGB.py
+++ Chapter1/6-SplitAndMergeRGB.py
@@ -11,6 +11,7 @@ image = cv2.imread('image.jpg')
 cv2.imshow('helloWorld' , image)
 
 # Getting Color values from the array
+# Splitting the Image based on the color value
 B ,G ,R  = cv2.split(image)
 print B, G, R
 cv2.imshow("Red" , R)
@@ -18,9 +19,46 @@ cv2.imshow("Blue" , B)
 cv2.imshow("Green" , G)
 
 
+cv2.waitKey()
+
+cv2.destroyAllWindows()
+#
+#
+# # MERGE Operations
+# # Lets get back the original Image
+merged = cv2.merge([B,G,R])
+cv2.imshow("Merge Image ", merged)
+
 
 cv2.waitKey()
 
 cv2.destroyAllWindows()
 
 
+# Increasing the intensity of blue color
+merged = cv2.merge([B+100, G, R])
+cv2.imshow("Merge Image with Amplified Blue Color", merged)
+
+cv2.waitKey()
+
+cv2.destroyAllWindows()
+
+
+
+## If you only need to filter to one color in image
+
+B,G,R = cv2.split(image)
+
+
+#print image.shape[:2]
+
+Zero = np.zeros(image.shape[:2] ,dtype= "uint8")
+print Zero
+
+cv2.imshow("BLUE", cv2.merge([B, Zero, Zero]))
+cv2.imshow("GREEN", cv2.merge([Zero, G, Zero]))
+cv2.imshow("RED", cv2.merge([Zero, Zero, R]))
+
+cv2.waitKey()
+
+cv2.destroyAllWindows()
